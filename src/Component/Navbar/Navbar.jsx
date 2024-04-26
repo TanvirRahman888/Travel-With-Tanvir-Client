eb5502336d74ef304b67c7eaba4ce73e20f620c8
Tanvir Module 1 <tanvirrahman8888@gmail.com>
2024-04-26 23:04:07 +0600

    Log in Complete

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const Navbar = () => {
-    const navLinks=<>
-    <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={'/'}>Home</Link></li>
-    <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={''}>All Tourists Spot</Link></li>
-    <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={''}>Add Tourists Spot</Link></li>
-    <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={''}>My List</Link></li>
-    <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={''}>About Us</Link></li>
-    <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={''}>Contact Us</Link></li>
+    const { user, logOut } = useContext(AuthContext);
+    const handelLogOut = () => {
+        logOut()
+            .then(() => alert("Welcome Back"))
+            .catch(() => alert("You are Here"))
+    }
+    const navLinks = <>
+        <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={'/'}>Home</Link></li>
+        <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={''}>All Tourists Spot</Link></li>
+        <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={''}>Add Tourists Spot</Link></li>
+        <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={''}>My List</Link></li>
+        <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={''}>About Us</Link></li>
+        <li className="text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={''}>Contact Us</Link></li>
     </>
     return (
         <div>
@@ -15,10 +23,10 @@ const Navbar = () => {
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg  className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        {navLinks}
+                            {navLinks}
                         </ul>
                     </div>
                     <img src="https://i.ibb.co/YT1X3Jr/a10-Logo2.png" className="w-20" alt="Logo" />
@@ -29,7 +37,12 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={'/login'}>Log In</Link></a>
+                    {user ?
+                        <a onClick={handelLogOut} className="btn text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3">Log Out</a>
+
+                        :
+                        <a className="btn text-xl border-2 border-blue-300 hover:border-blue-800 rounded-xl mx-3"><Link to={'/login'}>Log In</Link></a>
+                    }
                 </div>
             </div>
 
--- src/Component/Provider/AuthProvider.jsx
+++ src/Component/Provider/AuthProvider.jsx
@@ -1,20 +1,41 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import app from "../Firebase/Firebase.Confing";
-export const AuthContext=createContext(null)
+export const AuthContext = createContext(null)
 
-const AuthProvider = ({children}) => {
-    const [user,setUser]=useState(null) 
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
     const auth = getAuth(app);
 
-    const createUser =(email, password)=>{
-        return createUserWithEmailAndPassword(auth, email, password)
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const logIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
     }
 
-const authInfo={
-    user,
-    createUser,
-}
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log("User on the On Auth State Change", currentUser);
+            setUser(currentUser);
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+
+
+    const authInfo = {
+        user,
+        createUser,
+        logIn,
+        logOut
+    }
 
     return (
         <AuthContext.Provider value={authInfo}>
