685e772fed346692315b3a16606cdde9a29258e2
Tanvir Module 1 <tanvirrahman8888@gmail.com>
2024-04-26 11:43:42 +0600

    first commit

---
+++ src/Firebase/Firebase.Confing.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCagDGr4WWfI9cV9DkZXN5f7HEGdoocihk",
+  authDomain: "travel-with-tanvir.firebaseapp.com",
+  projectId: "travel-with-tanvir",
+  storageBucket: "travel-with-tanvir.appspot.com",
+  messagingSenderId: "522484064120",
+  appId: "1:522484064120:web:5baa7e6f8c2628002bf28d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/Router/Router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const Router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world --------?!</div>,
+    },
+    {
+      path: "/a",
+      element: <div>aaaaaaaaaaaa</div>,
+    },
+  ]);
+
+export default Router;
